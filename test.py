296651154d414e40c2d116ccd939ba01bae4901b
Alvaro Ramirez <alv.rsl@outlook.com>
2017-07-27 18:42:47 -0500

    Plus and Minus multidigit operations working

---
+++ test.py
@@ -0,0 +1,7 @@
+from calc1 import Interpreter
+
+interpreter = Interpreter('3+5')
+print(interpreter.get_next_token())
+print(interpreter.get_next_token())
+print(interpreter.get_next_token())
+print(interpreter.get_next_token())
